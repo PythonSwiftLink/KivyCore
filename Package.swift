9cb4ecfdded3e9975a87afb9f7387356c603b063
pythonswiftlink <pythonswiftlink@gmail.com>
2023-10-07 02:07:22 +0000

    Update from https://github.com/PythonSwiftLink/KivyCoreBuilder/commit/f22ed0752374d18c7a8c0e41e31720d128f3ccbc

--- Package.swift
+++ Package.swift
@@ -26,11 +26,11 @@ let package = Package(
     name: "KivyCore",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-		.library(name: "PythonLib", targets: ["KivyCore"]),
+		.library(name: "PythonCore", targets: ["PythonCore"]),
     ],
     targets: [
 		.target(
-			name: "KivyCore",
+			name: "PythonCore",
 			dependencies: pythonLibDeps,
 			
 			linkerSettings: [
