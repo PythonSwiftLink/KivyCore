e9d7da6f66d02c328d06b231091d67c82c0174d1
pythonswiftlink <pythonswiftlink@gmail.com>
2023-10-14 14:03:01 +0000

    Update from https://github.com/PythonSwiftLink/KivyCoreBuilder/commit/78bb0882c4d20f092f0b21f63ad134701f76a770

--- Package.swift
+++ Package.swift
@@ -3,94 +3,71 @@
 
 import PackageDescription
 
-var pythonLibDeps: [Target.Dependency] = [
-	"libcrypto",
-	"libpython3.10",
-	"libffi",
-	"libfreetype",
-	"libios",
-	"libjpeg",
-	"libkivy",
-	"libpillow",
-	"libpng16",
-	"libpyobjus",
-	"libSDL2_image",
-	"libSDL2_mixer",
-	"libSDL2_ttf",
-	"libSDL2",
-	"libssl"
-]
-
-
 let package = Package(
     name: "KivyCore",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-		.library(name: "PythonCore", targets: ["PythonCore"]),
+        .library(
+            name: "KivyCore",
+            targets: [
+                "KivyCore"
+            ]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/PythonSwiftLink/KivyPythonCore", from: .init(310, 0, 0)),
     ],
     targets: [
-		.target(
-			name: "PythonCore",
-			dependencies: pythonLibDeps,
-			
-			linkerSettings: [
-				.linkedLibrary("ncurses"),
-				//.linkedLibrary("ffi"),
-				.linkedLibrary("sqlite3"),
-				.linkedLibrary("z"),
-				//.linkedLibrary("panel"),
-				.linkedLibrary("bz2"),
-				
-				
-				// frameworks for sdl
-				.linkedFramework("OpenGLES"),
-				//.linkedFramework("AVFoundation"),
-				.linkedFramework("ImageIO"),
-				.linkedFramework("CoreHaptics"),
-				.linkedFramework("MobileCoreServices"),
-				.linkedFramework("MessageUI"),
-				.linkedFramework("WebKit"),
-				.linkedFramework("Accelerate"),
-				.linkedFramework("CoreGraphics"),
-				.linkedFramework("CoreVideo"),
-				.linkedFramework("AudioToolbox"),
-				.linkedFramework("GameController"),
-				.linkedFramework("Photos"),
+        .target(
+            name: "KivyCore",
+            dependencies: [
+                "PythonCore",
+                "libfreetype",
+                "libios",
+                "libjpeg",
+                "libkivy",
+                "libpillow",
+                "libpng16",
+                "libpyobjus",
+                "libSDL2_image",
+                "libSDL2_mixer",
+                "libSDL2_ttf",
+                "libSDL2",
+            ],
+            linkerSettings: [
+                // frameworks for sdl
+                .linkedFramework("OpenGLES"),
+                .linkedFramework("Metal"),
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("ImageIO"),
+                .linkedFramework("CoreHaptics"),
+                .linkedFramework("MobileCoreServices"),
+                .linkedFramework("MessageUI"),
+                .linkedFramework("WebKit"),
+                .linkedFramework("Accelerate"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("CoreVideo"),
+                .linkedFramework("CoreAudio"),
+                .linkedFramework("AudioToolbox"),
+                .linkedFramework("GameController"),
+                .linkedFramework("Photos"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("QuartzCore"),
+                .linkedFramework("CoreMotion"),
+                .linkedFramework("CoreMedia")
+            ]
+        ),
+        .binaryTarget(name: "libfreetype", path: "Resources/xcframework/libfreetype.zip"),
+        .binaryTarget(name: "libios", path: "Resources/xcframework/libios.zip"),
+        .binaryTarget(name: "libjpeg", path: "Resources/xcframework/libjpeg.zip"),
+        .binaryTarget(name: "libkivy", path: "Resources/xcframework/libkivy.zip"),
+        .binaryTarget(name: "libpillow", path: "Resources/xcframework/libpillow.zip"),
+        .binaryTarget(name: "libpng16", path: "Resources/xcframework/libpng16.zip"),
+        .binaryTarget(name: "libpyobjus", path: "Resources/xcframework/libpyobjus.zip"),
+        .binaryTarget(name: "libSDL2_image", path: "Resources/xcframework/libSDL2_image.zip"),
+        .binaryTarget(name: "libSDL2_mixer", path: "Resources/xcframework/libSDL2_mixer.zip"),
+        .binaryTarget(name: "libSDL2_ttf", path: "Resources/xcframework/libSDL2_ttf.zip"),
+        .binaryTarget(name: "libSDL2", path: "Resources/xcframework/libSDL2.zip"),
+        
 
-				.linkedFramework("UIKit"),
-				.linkedFramework("QuartzCore"),
-				
-				.linkedFramework("CoreMotion"),
-				
-
-				//.linkedLibrary("Metal"),
-				
-			]
-				
-		),
-		
-		.binaryTarget(name: "libcrypto", path: "Sources/PythonCore/xcframework/libcrypto.zip"),
-		.binaryTarget(name: "libpython3.10", path: "Sources/PythonCore/xcframework/libpython3.10.zip"),
-		.binaryTarget(name: "libffi", path: "Sources/PythonCore/xcframework/libffi.zip"),
-		.binaryTarget(name: "libfreetype", path: "Sources/PythonCore/xcframework/libfreetype.zip"),
-		.binaryTarget(name: "libios", path: "Sources/PythonCore/xcframework/libios.zip"),
-		.binaryTarget(name: "libjpeg", path: "Sources/PythonCore/xcframework/libjpeg.zip"),
-		.binaryTarget(name: "libkivy", path: "Sources/PythonCore/xcframework/libkivy.zip"),
-		.binaryTarget(name: "libpillow", path: "Sources/PythonCore/xcframework/libpillow.zip"),
-		.binaryTarget(name: "libpng16", path: "Sources/PythonCore/xcframework/libpng16.zip"),
-		.binaryTarget(name: "libpyobjus", path: "Sources/PythonCore/xcframework/libpyobjus.zip"),
-		.binaryTarget(name: "libSDL2_image", path: "Sources/PythonCore/xcframework/libSDL2_image.zip"),
-		.binaryTarget(name: "libSDL2_mixer", path: "Sources/PythonCore/xcframework/libSDL2_mixer.zip"),
-		.binaryTarget(name: "libSDL2_ttf", path: "Sources/PythonCore/xcframework/libSDL2_ttf.zip"),
-		.binaryTarget(name: "libSDL2", path: "Sources/PythonCore/xcframework/libSDL2.zip"),
-		.binaryTarget(name: "libssl", path: "Sources/PythonCore/xcframework/libssl.zip"),
-
-		// numoy
-		.binaryTarget(name: "libnumpy", path: "Sources/PythonCore/xcframework/libnumpy.zip"),
-		.binaryTarget(name: "libnpymath", path: "Sources/PythonCore/xcframework/libnpymath.zip"),
-		.binaryTarget(name: "libnpyrandom", path: "Sources/PythonCore/xcframework/libnpyrandom.zip"),
-//		.binaryTarget(name: "libcrypto", path: "xcframework/libcrypto.xcframework"),
-//		.binaryTarget(name: "libcrypto", path: "xcframework/libcrypto.xcframework"),
-//		
     ]
 )
