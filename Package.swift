2906f889dad8fded2e915e3538d93d7b4ac840bf
pythonswiftlink <pythonswiftlink@gmail.com>
2023-10-15 02:08:38 +0000

    Update from https://github.com/PythonSwiftLink/KivyCoreBuilder/commit/b3aee521d4a0b4af0c358358f745e267fb256911

--- Package.swift
+++ Package.swift
@@ -1,73 +1,65 @@
+
 // swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "KivyCore",
-    platforms: [.iOS(.v13)],
+    name: "KivyCore", 
     products: [
         .library(
-            name: "KivyCore",
+            name: "KivyCore", 
             targets: [
                 "KivyCore"
-            ]),
-    ],
+            ]), 
+    ], 
     dependencies: [
-        .package(url: "https://github.com/PythonSwiftLink/KivyPythonCore", from: .init(310, 0, 0)),
-    ],
+        .package(url: "https://github.com/PythonSwiftLink/KivyPythonCore", from: .init(310, 0, 0)), 
+    ], 
     targets: [
         .target(
-            name: "KivyCore",
+            name: "KivyCore", 
             dependencies: [
-                "PythonCore",
-                "libfreetype",
-                "libios",
-                "libjpeg",
-                "libkivy",
-                "libpillow",
-                "libpng16",
-                "libpyobjus",
-                "libSDL2_image",
-                "libSDL2_mixer",
-                "libSDL2_ttf",
-                "libSDL2",
-            ],
+                "PythonCore", 
+                "libfreetype", 
+                "libios", 
+                "libjpeg", 
+                "libkivy", 
+                "libpillow", 
+                "libpng16", 
+                "libpyobjus", 
+                "libSDL2_image", 
+                "libSDL2_mixer", 
+                "libSDL2_ttf", 
+                "libSDL2", 
+            ], 
             linkerSettings: [
                 // frameworks for sdl
-                .linkedFramework("OpenGLES"),
-                .linkedFramework("Metal"),
-                .linkedFramework("AVFoundation"),
-                .linkedFramework("ImageIO"),
-                .linkedFramework("CoreHaptics"),
-                .linkedFramework("MobileCoreServices"),
-                .linkedFramework("MessageUI"),
-                .linkedFramework("WebKit"),
-                .linkedFramework("Accelerate"),
-                .linkedFramework("CoreGraphics"),
-                .linkedFramework("CoreVideo"),
-                .linkedFramework("CoreAudio"),
-                .linkedFramework("AudioToolbox"),
-                .linkedFramework("GameController"),
-                .linkedFramework("Photos"),
-                .linkedFramework("UIKit"),
-                .linkedFramework("QuartzCore"),
-                .linkedFramework("CoreMotion"),
+                .linkedFramework("OpenGLES"), 
+                .linkedFramework("Metal"), 
+                .linkedFramework("AVFoundation"), 
+                .linkedFramework("ImageIO"), 
+                .linkedFramework("CoreHaptics"), 
+                .linkedFramework("MobileCoreServices"), 
+                .linkedFramework("MessageUI"), 
+                .linkedFramework("WebKit"), 
+                .linkedFramework("Accelerate"), 
+                .linkedFramework("CoreGraphics"), 
+                .linkedFramework("CoreVideo"), 
+                .linkedFramework("CoreAudio"), 
+                .linkedFramework("AudioToolbox"), 
+                .linkedFramework("GameController"), 
+                .linkedFramework("Photos"), 
+                .linkedFramework("UIKit"), 
+                .linkedFramework("QuartzCore"), 
+                .linkedFramework("CoreMotion"), 
                 .linkedFramework("CoreMedia")
             ]
-        ),
-        .binaryTarget(name: "libfreetype", path: "Resources/xcframework/libfreetype.zip"),
-        .binaryTarget(name: "libios", path: "Resources/xcframework/libios.zip"),
-        .binaryTarget(name: "libjpeg", path: "Resources/xcframework/libjpeg.zip"),
-        .binaryTarget(name: "libkivy", path: "Resources/xcframework/libkivy.zip"),
-        .binaryTarget(name: "libpillow", path: "Resources/xcframework/libpillow.zip"),
-        .binaryTarget(name: "libpng16", path: "Resources/xcframework/libpng16.zip"),
-        .binaryTarget(name: "libpyobjus", path: "Resources/xcframework/libpyobjus.zip"),
-        .binaryTarget(name: "libSDL2_image", path: "Resources/xcframework/libSDL2_image.zip"),
-        .binaryTarget(name: "libSDL2_mixer", path: "Resources/xcframework/libSDL2_mixer.zip"),
-        .binaryTarget(name: "libSDL2_ttf", path: "Resources/xcframework/libSDL2_ttf.zip"),
-        .binaryTarget(name: "libSDL2", path: "Resources/xcframework/libSDL2.zip"),
-        
-
+        ), 
+    	.binaryTarget(name: "libSDL2_mixer", url: "https://github.com/PythonSwiftLink/KivyCore/releases/download/310.9.11/libSDL2_mixer.zip", checksum: "libSDL2_ttf"), 
+    	.binaryTarget(name: "libpillow", url: "https://github.com/PythonSwiftLink/KivyCore/releases/download/310.9.11/libpillow.zip", checksum: "libpng16"), 
+    	.binaryTarget(name: "libpyobjus", url: "https://github.com/PythonSwiftLink/KivyCore/releases/download/310.9.11/libpyobjus.zip", checksum: "libSDL2_image"), 
+    	.binaryTarget(name: "libjpeg", url: "https://github.com/PythonSwiftLink/KivyCore/releases/download/310.9.11/libjpeg.zip", checksum: "libkivy"), 
+    	.binaryTarget(name: "libfreetype", url: "https://github.com/PythonSwiftLink/KivyCore/releases/download/310.9.11/libfreetype.zip", checksum: "libios"), 
     ]
 )
